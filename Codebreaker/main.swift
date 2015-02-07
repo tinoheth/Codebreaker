fb3107975fa17fd052d447f44bdd39657ef8901e
Tino Heth <th@t-no.de>
2015-02-07 18:17:47 +0100

    Editing, delete and create Breakpoints

--- Codebreaker/main.swift
+++ Codebreaker/main.swift
@@ -25,27 +25,57 @@ func walk(root: NSXMLElement) {
 }
 
 func main() {
-	println(NSFileManager.defaultManager().currentDirectoryPath)
-	print("Writing XML file ")
-	let url = NSURL(fileURLWithPath: "Submodules/LibXcbkptlist/LibXcbkptlistTests/Input/Breakpoints_v2.xcbkptlist")
-	let data = NSData(contentsOfURL: url!)
-	if let xml = NSXMLDocument(data: data!, options: 0, error: nil) {
-		let doc = BreakpointFile(xmlDocument: xml)
-		if let br = doc.fileBreakpoints.last {
-			br.ignoreCount = 44
-		}
-		if let target = NSURL(fileURLWithPath: "Breakpoints.xml") {
-			doc.toXMLDocument().XMLDataWithOptions(Int(NSXMLNodePrettyPrint)).writeToURL(target, atomically: true)
+	let directory = NSFileManager.defaultManager().currentDirectoryPath
+	println("Running in directory \(directory)")
+	var targetURL: NSURL?
+	var sourceFiles = [NSURL]()
+	for argument in Process.arguments[1..<Process.arguments.count] {
+		let type = argument.pathExtension
+		if type == "m" {
+			if let url = NSURL(fileURLWithPath: argument) {
+				sourceFiles.append(url)
+			}
+		} else if type == "xcbkptlist" {
+			targetURL = NSURL(fileURLWithPath: argument)
+		} else if type == "xcworkspace" || type == "xcodeproj" {
+			if let user = NSProcessInfo.processInfo().environment["USER"] as? String {
+				targetURL = NSURL(fileURLWithPath: directory.stringByAppendingPathComponent(argument).stringByAppendingPathComponent("xcuserdata/\(user).xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist"))
+			}
 		}
 	}
-
-//	if let doc = parse(NSURL(fileURLWithPath: "/Users/tinoheth/Projekte/Coolspot/Coolspot.xcworkspace/xcuserdata/tinoheth.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist")) {
-//	switch doc {
-//	case .success(data: let xml):
-//		walk(xml.rootElement()!)
-//	default: break
-//	}
-//	}
+	if let targetURL = targetURL {
+		println("Using file \(targetURL)")
+		if let data = NSData(contentsOfURL: targetURL) {
+			if let xml = NSXMLDocument(data: data, options: 0, error: nil) {
+				let doc = BreakpointFile(xmlDocument: xml)
+				let list = doc.fileBreakpointsForPath(doc.registeredFiles().first)
+				if let br = list.last {
+					br.ignoreCount = 44
+				}
+				if let br = list.first {
+					doc.deleteBreakpoint(br)
+					br.startingLineNumber = 33
+					doc.addFileBreakpoint(br)
+				}
+				doc.toXMLDocument().XMLDataWithOptions(Int(NSXMLNodePrettyPrint)).writeToURL(targetURL, atomically: true)
+			}
+		}
+		
+		if let components = targetURL.pathComponents as? [String] {
+			let last = components.count - 5
+			if last > 0 {
+				let start = NSURL(fileURLWithPath: "/")!
+				let container = components[0...last].reduce(start) { url, component in
+					return url.URLByAppendingPathComponent(component)
+				}
+				let projectFile = container.URLByAppendingPathComponent("project.pbxproj")
+				var err: NSError?
+				if !NSFileManager.defaultManager().setAttributes([NSFileModificationDate: NSDate()], ofItemAtPath: projectFile.path!, error: &err) {
+					println(err)
+				}
+			}
+		}
+	}
 }
 
 main()
