c5bf1879f2df320208f39c28a6bc86bef4bb81f1
Tino Heth <th@t-no.de>
2015-04-27 10:24:11 +0200

    Non-crashing test

---
+++ Testproject/Test/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Tino Heth on 18.04.15.
+//  Copyright (c) 2015 Tino Heth. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+#if codebreak=true && ignore=10 && enabled=true && MyTag && tag=Mark
+	println("Look at me")
+	println("I'm a breakpoint action")
+	println("No self in here")
+#endif
+println("I'm done")
+println("(really)")
+
